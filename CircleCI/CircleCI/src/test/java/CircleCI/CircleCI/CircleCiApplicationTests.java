48754880822ea4eed79192fe5bbb3ef1c7827489
jpcabana1 <jpcabana1@gmail.com>
2021-11-20 09:22:07 -0300

    setup

---
+++ CircleCI/CircleCI/src/test/java/CircleCI/CircleCI/CircleCiApplicationTests.java
@@ -0,0 +1,13 @@
+package CircleCI.CircleCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
